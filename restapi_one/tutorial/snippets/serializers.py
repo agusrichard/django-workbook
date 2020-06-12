a8b6a74888b9b2d3e3bb009bf5d4e1d37d47b623
agusrichard <agus.richard21@gmail.com>
2020-06-12 20:31:31 +0700

    Authentication and Permissions

--- restapi_one/tutorial/snippets/serializers.py
+++ restapi_one/tutorial/snippets/serializers.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        fields = ['id', 'username', 'snippets']
