b0cc4c1ed1db8fce9e957b8fb29d23f20da88dae
agusrichard <agus.richard21@gmail.com>
2020-06-15 14:58:15 +0700

    Create model

--- restapi_two/todos/api/models.py
+++ restapi_two/todos/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Todo(models.Model):
+    created = models.DateField()
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.content
