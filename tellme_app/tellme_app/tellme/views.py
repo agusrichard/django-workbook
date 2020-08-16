92ebe900cdfdc40d00045eaa19e9637742e5ad86
agusrichard <agus.richard21@gmail.com>
2020-08-16 17:10:41 +0700

    Create Base HTML Layout

--- tellme_app/tellme_app/tellme/views.py
+++ tellme_app/tellme_app/tellme/views.py
@@ -5,7 +5,7 @@ from .models import User
 # Create your views here.
 def index(request):
     users = User.objects.all()
-    return render(request, 'tellme/index.html', { 'users': users })
+    return render(request, 'tellme/base.html', { 'users': users })
 
 def user(request, user_id):
     user = User.objects.get(pk=user_id)
