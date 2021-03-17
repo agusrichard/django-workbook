8834dd118b2caac2bc5c4da6c89ef5c368f4fb55
agusrichard <agus.richard21@gmail.com>
2021-03-17 22:29:17 +0700

    Basic serialization

--- recipies/mysite/recipies/models.py
+++ recipies/mysite/recipies/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class DateTimeInfo(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -9,17 +8,16 @@ class DateTimeInfo(models.Model):
         abstract = True
         ordering = ['created_date']
 
-
 class Restaurant(DateTimeInfo):
     name = models.CharField(max_length=64)
     address = models.TextField()
 
+class ProductType(models.IntegerChoices):
+    FOOD = 1
+    BEVERAGES = 2
+    OTHERS = 3
 
 class Product(DateTimeInfo):
-    class ProductType(models.IntegerChoices):
-        FOOD = 1
-        BEVERAGES = 2
-        OTHERS = 3
 
     product_type = models.IntegerField(choices=ProductType.choices)
     name = models.CharField(max_length=128)
