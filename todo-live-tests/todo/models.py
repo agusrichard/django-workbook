7bcd5e4e31bd174bba947c98717796778c171ef4
Agus Richard Lubis <agus.richard21@gmail.com>
2022-05-03 05:30:35 +0700

    Start creating todo live tests for article material

---
+++ todo-live-tests/todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
